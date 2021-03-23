8412dbd62fb6a99a549ec2bb0eca0a4021013667
Trevor Wagner <twagner.git@gmail.com>
2021-03-22 23:04:38 -0500

    Adds an abstract parent class for test suites

---
+++ src/main/java/io/twagner/spiegel/framework/suites/SuiteParent.java
@@ -0,0 +1,8 @@
+package io.twagner.spiegel.framework.suites;
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public abstract class SuiteParent {
+}
