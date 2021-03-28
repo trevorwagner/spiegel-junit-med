1877eb6d7ef49b099da419d020094bd5d20f566d
Trevor Wagner <twagner.git@gmail.com>
2021-03-28 00:39:38 -0500

    Adds support class that converts text to boolean

---
+++ src/main/java/io/twagner/spiegel/cucumber/glue/support/languages/en/Expectation.java
@@ -0,0 +1,29 @@
+package io.twagner.spiegel.cucumber.glue.support.languages.en;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Expectation {
+    public boolean it;
+
+    private final List<String> trueValues = Arrays.asList("true", "should");
+    private final List<String> falseValues = Arrays.asList("false", "should not");
+
+    private boolean parseValue(String provided) {
+        if (!trueValues.contains(provided)) {
+            if (!falseValues.contains(provided)) {
+                throw new Error("The provided string ( "
+                        + provided
+                        + " ) does not match either list defining true- or false values");
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public Expectation(String provided) {
+        this.it = parseValue(provided);
+    }
+}
