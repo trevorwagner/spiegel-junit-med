ead45a355dd58d3449116a4339856a21980ba985
Trevor Wagner <twagner.git@gmail.com>
2021-03-28 00:33:09 -0500

    Updates base glue class to use Spring Autowiring
    This also provides a teardown method that quits the WebDriver after a
    test run has completed.
    
    I believe this also introduces the WebDriver- and frameworkConfig fields
    for the first time within glue code.

--- src/main/java/io/twagner/spiegel/cucumber/glue/SpringBootIntegrationTest.java
+++ src/main/java/io/twagner/spiegel/cucumber/glue/SpringBootIntegrationTest.java
@@ -1,11 +1,31 @@
 package io.twagner.spiegel.cucumber.glue;
 
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.twagner.spiegel.framework.configuration.SpiegelConfiguration;
 import io.twagner.spiegel.framework.context.SpiegelFrameworkContext;
+import org.junit.jupiter.api.AfterAll;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
+@SpringBootTest
 @CucumberContextConfiguration
-@SpringBootTest(classes = SpiegelFrameworkContext.class)
+@ContextConfiguration(classes = SpiegelFrameworkContext.class)
 public abstract class SpringBootIntegrationTest {
     /* All glue code classes should subclass this abstract class */
+    @Autowired
+    protected WebDriver webDriver;
+
+    @Autowired
+    protected SpiegelConfiguration frameworkConfig;
+
+    @AfterAll
+    static void frameworkTeardown() {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpiegelFrameworkContext.class);
+        WebDriver driver = ctx.getBean(WebDriver.class);
+        driver.quit();
+    }
 }
