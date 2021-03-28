5d7e6da0d24589fd1c942f5a222616bbf7bc4561
Trevor Wagner <twagner.git@gmail.com>
2021-03-28 00:38:15 -0500

    Adds page object for DuckDuckGo home page

---
+++ src/main/java/io/twagner/spiegel/pageobjects/duckduckgo/DuckDuckGoHomePage.java
@@ -0,0 +1,28 @@
+package io.twagner.spiegel.pageobjects.duckduckgo;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class DuckDuckGoHomePage {
+    public static final String PAGE_URL = "https://duckduckgo.com/";
+
+    private static final String XPATH_SEARCH_FORM = "//form[@id='search_form_homepage']";
+
+    @CacheLookup
+    @FindBy(xpath = "//a[@id='logo_homepage_link']")
+    public WebElement homePageLink;
+
+    @CacheLookup
+    @FindBy(xpath = XPATH_SEARCH_FORM + "/input[@id='search_form_input_homepage']")
+    public WebElement searchField;
+
+    @CacheLookup
+    @FindBy(xpath = XPATH_SEARCH_FORM + "/input[@id='search_button_homepage']")
+    public WebElement searchButton;
+
+    public DuckDuckGoHomePage enterSearchString(String searchQuery){
+        this.searchField.sendKeys(searchQuery);
+        return this;
+    }
+}
