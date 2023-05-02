0272a1bca7393f572b4c8ef85091c55fcd9cb729
Trevor Wagner <twagner.git@gmail.com>
2023-05-02 10:56:31 -0500

    Updates XPath on Page Object
    For some reason the DOM displays differently in ChromeDriver than in
    Chrome Desktop.  I've added notes.

--- src/main/java/io/twagner/spiegel/pageobjects/duckduckgo/DuckDuckGoHomePage.java
+++ src/main/java/io/twagner/spiegel/pageobjects/duckduckgo/DuckDuckGoHomePage.java
@@ -4,13 +4,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
+/* For some reason right now when this page is loaded in Chromedriver,
+   different DOM elements (like for homePageLogo -- see below). than
+   what display within Chrome desktop.  For a demonstration framework
+   I have no problem working around this, but if if this was a test
+   framework used by a delivery team I expect that I might try
+   inquiring into the difference.
+ */
 public class DuckDuckGoHomePage {
     public static final String PAGE_URL = "https://duckduckgo.com/";
 
     private static final String XPATH_SEARCH_FORM = "//form[@id='searchbox_homepage']";
 
     @CacheLookup
-    @FindBy(xpath = "//div[contains(@class, 'header_logoStacked')]/img")
+    // xpath for Chrome desktop: "//div[contains(@class, 'header_logoStacked')]/img"
+    @FindBy(xpath = "//section[contains(@class, 'legacy-homepage_searchSection')]/img")
     public WebElement homePageLogo;
 
     @CacheLookup
